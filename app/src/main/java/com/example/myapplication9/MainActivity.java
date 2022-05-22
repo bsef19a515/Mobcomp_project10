da525ea0aee2bdd80782457c29cde5f71e71d204
kinza <bsef19a515@pucit.edu.pk>
2022-05-22 16:29:35 +0500

    activity1,2 design

--- app/src/main/java/com/example/myapplication9/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dashboard(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+    public void home(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
